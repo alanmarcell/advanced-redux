6233645adcd77afbd4dc0990fc69f278e132e552
alanmarcell <alanmarcell@live.com>
2017-11-09 15:08:40 -0200

    Middleware complete

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ import webpackConfig from './../webpack.config'
 import webpackDevMiddleware from 'webpack-dev-middleware';
 const compiler = webpack(webpackConfig);
 import webpackHotMiddleware from "webpack-hot-middleware";
+import socketIO from 'socket.io'
+import { simulateActivity } from './simulateActivity';
 
 import {
     channels,
@@ -17,6 +19,7 @@ import {
 
 let app = express();
 const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(cors());
 app.use(webpackDevMiddleware(compiler, {
@@ -112,5 +115,7 @@ app.use(express.static('public/css'));
 
 const port = 9000;
 server.listen(port,()=>{
-    console.info(`Redux Messenger is listening on port ${port}.`);
+    console.info(`Redux Messenger is listening on port => ${port}.`);
 });
+
+simulateActivity(currentUser.id);
